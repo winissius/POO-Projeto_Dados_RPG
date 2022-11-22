ada867441c418e947c5dcced0726c4932dea4bf7
winissius <winissius.m@gmail.com>
2022-11-22 19:05:35 -0300

    Adição de interface gráfica. Adição de diagramas UML.

--- src/Dados.java
+++ src/Dados.java
@@ -1,6 +1,8 @@
 import java.io.Serializable;
 
-public abstract class Dados{
+public abstract class Dados implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     private String cor;
     protected int lados;
     private String dono;
@@ -12,33 +14,21 @@ public abstract class Dados{
 
     public Dados(String cor, String dono, int quantidade) {
         this.cor = cor;
-        // this.lados = lados;
         this.dono = dono;
         this.quantidade = quantidade;
     }
 
     public String informacoes(){
         String info = "";
-        info += "Dado de " + lados + " lados\n";
-        info += "Cor do dado é " + cor +"\n";
-        info += "O dono do dado é " + dono + "\n";
-        info += "Ao todo são " + quantidade + " dados\n";
+        info += "Dado de " + this.lados + " lados\n";
+        info += "Cor do dado é " + this.cor +"\n";
+        info += "O dono do dado é " + this.dono + "\n";
+        info += "Ao todo são " + this.quantidade + " dados\n";
         info += "Suas rolagens foram: " +  rolar();
         return info;
     }
 
     public abstract String rolar();
-
-    public static void main(String[] args) {
-        Dados dado1 = new D6("Azul", "Winissius", 3);
-        System.out.println(dado1.informacoes());
-        System.out.println("=============================");
-        Dados dado2 = new D12("Dourado", "Deds Maria", 50);
-        System.out.println(dado2.informacoes());
-        System.out.println("=============================");
-        Dados dado3 = new D20("Preto", "Mingoso", 1);
-        System.out.println(dado3.informacoes());
-    }
 
 
 }
